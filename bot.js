7e40a6f720036c74e75ba013657777e9832c0d64
Waisu <85125438+Waisu242@users.noreply.github.com>
2021-06-10 20:40:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const moment = require("moment");
 const chalk = require("chalk");
 
 let waisu;
+const register = "RegisterRoleID";
+const unregistered = "UnregisteredRoleID";
 const tokens = ["BotToken1", "BotToken2", "BotToken3", "BotToken4", "BotToken5"]; // İstediğin Kadar Arttırabilirsin!
 const channels = ["ConfirmationChannelsID1", "ConfirmationChannelsID2", "ConfirmationChannelsID3", "ConfirmationChannelsID4", "ConfirmationChannelsID5"]; // İstediğin Kadar Arttırabilirsin!
 
@@ -17,10 +19,33 @@ console.log(chalk `{greenBright [${moment().format('YYYY-MM-DD HH:mm:ss')}]} {re
 });
 
 client.on("voiceStateUpdate", async (olds, news) => {
-if(news.member.id === client.user.id) waisu = await client.channels.cache.get(channels[index]).join();
-});
 
-// Ses Sistemi Yakında!
+if(news.member.id === client.user.id) waisu = await client.channels.cache.get(channels[index]).join();
+
+let confirmation = client.channels.cache.get(channels[index]); 
+confirmation.join().then(connection => { 
+    
+if(news.channelID === channels[index]) {
+if(news.member.roles.cache.get(unregistered)) {
+
+const last = connection.play("./sound/hg.mp3");
+last.on("end", () => {
+last.destroy();
+return;
+});
+};
+
+if(news.member.permissions.has("ADMINISTRATOR") || news.member.roles.cache.get(register)) {
+const last = connection.play("./sound/yetkili.mp3");
+last.on("end", () => {
+last.destroy();
+return;
+});
+};
+
+};
+});
+});
 
 client.login(tokens[index]);
 };
